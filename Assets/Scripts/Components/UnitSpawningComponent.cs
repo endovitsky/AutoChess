c95250bf109bfa534c2be2c3fa10599abd4bd94f
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2020-02-28 10:35:22 +0300

    Add spawning of units on squares

---
+++ Assets/Scripts/Components/UnitSpawningComponent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Components
+{
+    public class UnitSpawningComponent : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform _unitPrefab;
+
+        private Transform _unitInstance;
+
+        private void Start()
+        {
+            _unitInstance = Instantiate(_unitPrefab, this.gameObject.transform);
+        }
+    }
+}
