85a32dc8151024eea3f637fef893e03307f4d660
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2020-02-28 08:17:21 +0300

    Reafactor game object manager

--- Assets/Scripts/Managers/GameObjectsManager.cs
+++ Assets/Scripts/Managers/GameObjectsManager.cs
@@ -14,10 +14,13 @@ namespace Managers
         {
             _chessBoardInstance = Instantiate(_chessBoardPrefab, this.gameObject.transform);
 
+            // set board position to center of parent container
             var size = _chessBoardInstance._squarePrefab.gameObject.GetComponent<SpriteRenderer>().bounds.size;
+            var xCorrection = GameManager.Instance.ChessBoardConfigurationService.Width / 2 + size.x / 2;
+            var yCorrection = GameManager.Instance.ChessBoardConfigurationService.Height / 2 + size.y / 2;
             _chessBoardInstance.gameObject.transform.localPosition = new Vector3(
-                _chessBoardInstance.gameObject.transform.localPosition.x - GameManager.Instance.ChessBoardConfigurationService.Width / 2 + size.x / 2,
-                _chessBoardInstance.gameObject.transform.localPosition.y - GameManager.Instance.ChessBoardConfigurationService.Height / 2 + size.y / 2);
+                _chessBoardInstance.gameObject.transform.localPosition.x - xCorrection,
+                _chessBoardInstance.gameObject.transform.localPosition.y - yCorrection);
 
             _chessBoardInstance.Initialize();
         }
