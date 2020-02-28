bcf071d1bb0c7fbbaeea8b93b602ab03f16d1161
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2020-02-28 09:32:33 +0300

    Add start battle window functional

---
+++ Assets/Scripts/Components/IsGameStateActivationComponent.cs
@@ -0,0 +1,29 @@
+﻿using Managers;
+using Services;
+using UnityEngine;
+
+namespace Components
+{
+    public class IsGameStateActivationComponent : MonoBehaviour
+    {
+        [SerializeField] 
+        private GameStateService.GameState _gameState;
+
+        private void Start()
+        {
+            GameManager.Instance.GameStateService.SelectedGameStateChanged += SelectedGameStateChanged;
+        }
+        private void OnDestroy()
+        {
+            if (GameManager.Instance.GameStateService.SelectedGameStateChanged != null)
+            {
+                GameManager.Instance.GameStateService.SelectedGameStateChanged -= SelectedGameStateChanged;
+            }
+        }
+
+        private void SelectedGameStateChanged(GameStateService.GameState gameState)
+        {
+            this.gameObject.SetActive(gameState == _gameState);
+        }
+    }
+}
---
+++ Assets/Scripts/Components/StartBattleButtonComponent.cs
@@ -0,0 +1,25 @@
+﻿using Managers;
+using Services;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Components
+{
+    [RequireComponent(typeof(Button))]
+    public class StartBattleButtonComponent : MonoBehaviour
+    {
+        private void Start()
+        {
+            this.gameObject.GetComponent<Button>().onClick.AddListener(OnClick);
+        }
+        private void OnDestroy()
+        {
+            this.gameObject.GetComponent<Button>().onClick.RemoveAllListeners();
+        }
+
+        private void OnClick()
+        {
+            GameManager.Instance.GameStateService.SelectedGameState = GameStateService.GameState.Started;
+        }
+    }
+}
