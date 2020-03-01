aa3bd55c731483b47c32cf3fe44dffddc22dd60a
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2020-03-01 11:54:01 +0300

    Add units texture assigning depend on team name

--- Assets/Scripts/Factories/UnitFactory.cs
+++ Assets/Scripts/Factories/UnitFactory.cs
@@ -9,18 +9,39 @@ namespace Factories
         [SerializeField]
         private Transform _unitPrefab;
 
-        private List<Transform> _unitInstances = new List<Transform>();
+        private List<Transform> _redTeamUnitInstances = new List<Transform>();
+        private List<Transform> _blueTeamUnitInstances = new List<Transform>();
 
         public void TrySpawnUnit(Transform square)
         {
+            var teamName = "";
+
             var rightBorderPositionX = GameManager.Instance.ChessBoardConfigurationService.Width;
             var leftBorderPositionX = 0;
-            if (square.localPosition.x - leftBorderPositionX <= GameManager.Instance.UnitsSpawningConfigurationService.StepFromLeft || // near the left border
-                rightBorderPositionX - square.localPosition.x < GameManager.Instance.UnitsSpawningConfigurationService.StepFromRight) // near the right border
+
+            if (square.localPosition.x - leftBorderPositionX <=
+                GameManager.Instance.UnitsSpawningConfigurationService.StepFromLeft) // near the left border
             {
                 var instance = Instantiate(_unitPrefab, square.gameObject.transform);
 
-                _unitInstances.Add(instance);
+                teamName = "Blue";
+
+                instance.gameObject.GetComponent<SpriteRenderer>().sprite = 
+                    GameManager.Instance.TexturesResourcesManager.Get("Units", teamName);
+
+                _blueTeamUnitInstances.Add(instance);
+            }
+            if (rightBorderPositionX - square.localPosition.x <
+                GameManager.Instance.UnitsSpawningConfigurationService.StepFromRight) // near the right border
+            {
+                var instance = Instantiate(_unitPrefab, square.gameObject.transform);
+
+                teamName = "Red";
+
+                instance.gameObject.GetComponent<SpriteRenderer>().sprite =
+                    GameManager.Instance.TexturesResourcesManager.Get("Units", teamName);
+
+                _redTeamUnitInstances.Add(instance);
             }
         }
     }
