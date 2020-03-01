436f6ec352fbe5bd0bc1a3fd7d904b70c8d86f01
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2020-03-01 21:41:41 +0300

    Add health bar to unit

---
+++ Assets/Scripts/Components/CanvasMainCameraSetterComponent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Components.SetterComponents
+{
+    public class CanvasMainCameraSetterComponent : MonoBehaviour
+    {
+        private void Awake()
+        {
+            this.gameObject.GetComponent<Canvas>().worldCamera = Camera.main;
+        }
+    }
+}
---
+++ Assets/Scripts/Components/HealthBarComponent.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Components
+{
+    public class HealthBarComponent : MonoBehaviour
+    {
+
+    }
+}
