bb7913e8be190a9864a81a0f67edbb30155b8bf7
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2020-03-03 10:16:46 +0300

    Add game over window

---
+++ Assets/Scripts/Components/RestartGameButtonComponent.cs
@@ -0,0 +1,29 @@
+﻿using Managers;
+using Services;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Components
+{
+    [RequireComponent(typeof(Button))]
+    public class RestartGameButtonComponent : MonoBehaviour
+    {
+        private void Start()
+        {
+            this.gameObject.GetComponent<Button>().onClick.AddListener(OnClick);
+        }
+        private void OnDestroy()
+        {
+            this.gameObject.GetComponent<Button>().onClick.RemoveAllListeners();
+        }
+
+        private void OnClick()
+        {
+            GameManager.Instance.GameStateService.SelectedGameState = GameStateService.GameState.NotStarted;
+
+            var scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+}
