2c4794d1e6db22595ad208aa4c377ac9a912e531
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2020-03-04 12:57:21 +0300

    Move logic for destroying dead unit to separate component

--- Assets/Scripts/Components/IsDeadUnitDestroyComponent.cs
+++ Assets/Scripts/Components/IsDeadUnitDestroyComponent.cs
@@ -4,24 +4,14 @@ using Views;
 
 namespace Components
 {
-    public class HealthBarComponent : MonoBehaviour
+    [RequireComponent(typeof(UnitView))]
+    public class IsDeadUnitDestroyComponent : MonoBehaviour
     {
-        [SerializeField]
-        private Image _healthBar;
-
         private UnitView _unitView;
-
-        private float _startHealth;
 
         private void Start()
         {
             _unitView = this.gameObject.GetComponentInParent<UnitView>();
-            if (_unitView == null)
-            {
-                Debug.LogError("No UnitView was found in parent game object");
-            }
-
-            _startHealth = _unitView.UnitModel.Health;
 
             OnHealthChanged(_unitView.UnitModel.Health);
             _unitView.UnitModel.HealthChanged += OnHealthChanged;
@@ -36,7 +26,10 @@ namespace Components
 
         private void OnHealthChanged(float health)
         {
-            _healthBar.fillAmount = health / _startHealth;
+            if (health <= 0)
+            {
+                Destroy(_unitView.gameObject);
+            }
         }
     }
 }
